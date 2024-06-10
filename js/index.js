b4a9404c99b7871713441a4e289899bde260cd5c
Agnes Odame <agnesodame5@gmail.com>
2024-06-10 09:42:20 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+const headerbtn = document.getElementById('headerbtn')
+headerbtn.onclick = function(){
+    alert('Lorem ipsum dolor sit amet consectetur adipisicing elit. Et praesentium eos nulla qui commodi consectetur beatae fugiat. Veniam iste rerum perferendis.')
+}
+
+
+const sec1btn = document.getElementById('sec1btn')
+sec1btn.onclick = function(){
+    alert('Lorem ipsum dolor sit amet consectetur adipisicing elit. Et praesentium eos nulla qui commodi consectetur beatae fugiat. Veniam iste rerum perferendis.')
+}
+
